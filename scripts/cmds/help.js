33e914dabb0df723bc8c0f8cc3b6153a39577c0a
mahi-khan0 <161739658+mahi-khan0@users.noreply.github.com>
2024-03-21 14:17:57 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ ⚠ | 𝐀𝐒𝐈𝐅 𝐱𝟔𝟗 ]";
+const doNotDelete = "[ ☀︎︎ | ☻︎𝐀𝐘𝐀𝐍☻︎ ]";
 /** 
 * @author NTKhang
 * @author: do not delete it
